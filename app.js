681607e99c0adc31524be3bb553fe5b0a6b129a8
Charles <charles0527@hotmail.com>
2022-04-18 02:23:39 +0800

    fix: syntax error

--- app.js
+++ app.js
@@ -3,39 +3,40 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const flash = require ('connect-flash')
+const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 const routes = require('./routes')
-// 載入一包 Passport 設定檔
 const usePassport = require('./config/passport')
 
 const app = express()
-const PORT = process.env.PORT || 3000
-
-usePassport(app)
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.use(flash)
+usePassport(app)
+
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
-  res.locals.success_msg = req.flash ('success_msg')
-  res.locals.warning_msg = req.flash ('warning_msg')
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
